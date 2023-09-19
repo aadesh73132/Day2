0adf42675122e15887aa7da6702a492a853f4fb4
aadesh73132 <145355002+aadesh73132@users.noreply.github.com>
2023-09-19 19:37:50 +0530

    Add files via upload

---
+++ add2.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+
+int main()
+{
+float a,b;
+a = 4.4;
+b = 5.5;
+
+
+printf("a+b=%f\n",a+b);
+printf("a-b=%f\n",a-b);
+printf("a*b=%f\n",a*b);
+printf("a/b=%f\n",a/b);
+
+return 0;
+}     
